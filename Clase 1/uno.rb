fc353479685b512630e6494e692c7bccf53ab76a
Victor Arriagada Martinez <veamchile@gmail.com>
2021-08-26 20:49:46 -0400

    Variables, concatenacion, to_i

--- Clase 1/uno.rb
+++ Clase 1/uno.rb
@@ -13,3 +13,32 @@ end
 
 texto = gets.chop
 uno(texto)
+
+
+
+=begin
+    Nombres de variables
+    casa, carro, proyecto
+    schedule_lessons
+=end
+
+puts 8.class
+puts "vitoko".class
+var = "Variable1"
+puts var.class
+
+num = 8
+puts "2".to_i + 8
+
+
+name1 = "hola"
+name1 += " vitoko"
+puts name1
+
+name2 = "hola"
+name2 = name2 + " vitoko"
+puts name2
+
+name3 = "hola"
+name3 << " vitoko"
+puts name3
