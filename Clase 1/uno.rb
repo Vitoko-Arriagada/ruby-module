688e20a538faff1063c1158ffbddc7b0b349db75
Victor Arriagada Martinez <veamchile@gmail.com>
2021-08-26 21:06:27 -0400

    Operaciones matematicas

--- Clase 1/uno.rb
+++ Clase 1/uno.rb
@@ -42,3 +42,27 @@ puts name2
 name3 = "hola"
 name3 << " vitoko"
 puts name3
+
+var1 = 10
+var2 = "100"
+var3 = 3.99
+var4 = -50
+
+puts var1 + var2.to_i
+puts var1.to_s + var2
+puts var1 / var3
+puts var4 + var3 - var1
+puts "--------------------"
+puts 2 * 3 + 4 - 5 / 2
+puts 2.0 * 3.0 + 4.0 - 5.0 / 2
+puts 2 * ((3 + 4) - 5) / 3
+puts 2.0 * ((3 + 4) - 5) / 3
+puts 10 ** 3
+puts Math.sqrt(25)
+
+def map(var, min_in, max_in, min_out, max_out)
+    ((((var - min_in)*(max_out - min_out))/(max_in - min_in)) + min_out)
+end
+
+valor = map(512,0,1024,0,180)
+puts valor
